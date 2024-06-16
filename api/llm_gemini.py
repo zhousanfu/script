a94daae10a2571866eae84500d5c34ec7b69038e
sanford <courageux_san@wechat.com>
2024-06-16 10:35:04 +0800

    up

--- api/llm_gemini.py
+++ api/llm_gemini.py
@@ -3,15 +3,70 @@
 '''
  Author: Sanfor Chow
  Date: 2024-04-25 20:18:20
- LastEditors: Sanfor Chow
- LastEditTime: 2024-04-26 22:22:04
+ LastEditors: courageux_san WX
+ LastEditTime: 2024-06-16 10:29:36
  FilePath: /script/api/llm_gemini.py
 '''
 import os
+import requests
+import PIL.Image
+from io import BytesIO
 import google.generativeai as genai
+from typing import Any, List, Mapping, Optional
+from langchain.llms.base import LLM
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+import requests
 
-GENAI_API_KEY = os.getenv('genai_api_key')
+GENAI_API_KEY = os.getenv('GENAI_API_KEY')
 ai_max_length = 1000
+
+class CustomLLM(LLM):
+    url: str
+    api_key: str
+    mode_name: str
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+    ) -> str:
+        if stop is not None:
+            raise ValueError("stop kwargs are not permitted.")
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer "+ self.api_key
+        }
+        data = {
+            "model": self.mode_name,
+            "messages": [{"role": "user", "content": prompt}],
+            "temperature": 0.7
+        }
+        response = requests.post(url=self.url, headers=headers, json=data)
+        return response.json()['choices'][0]['message']['content']
+
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        """Get the identifying parameters."""
+        return {"url": self.url}
+
+def genai_proxy(url, api_key, mode_name, prompt):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer "+ api_key
+    }
+    data = {
+        "model": mode_name,
+        "messages": [{"role": "user", "content": prompt}],
+        "temperature": 0.7
+    }
+    response = requests.post(url=url, headers=headers, json=data)
+    return response.json()['choices'][0]['message']['content']
 
 def split_text_into_chunks(text, max_length=ai_max_length):
     """
@@ -33,7 +88,7 @@ def rewrite_text_with_genai(text, prompt="Please rewrite this text:"):
     chunks = split_text_into_chunks(text)
     rewritten_text = ''
     # pbar = tqdm(total=len(chunks), ncols=150)
-    genai.configure(api_key=os.getenv('genai_api_key'))
+    genai.configure(api_key=GENAI_API_KEY)
     model = genai.GenerativeModel('gemini-pro')
     for chunk in chunks:
         _prompt=f"{prompt}\n{chunk}",
@@ -73,8 +128,37 @@ def rewrite_text_with_genai(text, prompt="Please rewrite this text:"):
     # pbar.close()
     return rewritten_text
 
+def img_genai(image_url, prompt):
+    r = requests.get(image_url)
+    image = PIL.Image.open(BytesIO(r.content))
+    image.save('api/image2.png', 'PNG')
+    image = PIL.Image.open('api/image.png')
+    genai.configure(api_key=GENAI_API_KEY)
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content(image)
+    response = model.generate_content([prompt, image], stream=True)
+    response.resolve()
+
+    return response.text
+
 
 
 if __name__ == '__main__':
-    res = rewrite_text_with_genai(text='', prompt='你是谁')
+    # # genai直连 文字
+    # res = rewrite_text_with_genai(text='', prompt='你是谁')
+
+    # # genai直连 多媒体
+    # res = img_genai(
+    #     image_url='https://mmilabel-1258344707.cos.ap-guangzhou.myqcloud.com/upload/unpack/cardli/20240507/image/1005129185_b1d78a1a84f376cdf76980885253c666.jpg_ocrmt_new.jpg',
+    #     prompt="""你需要使用一句英文句子来总结图片中的核心内容, 句子类似于图片标题一样的, 如果是一些产品图片只用说明产品是什么就行。 要求: 1. 句子要简洁明了，能够体现出主图片的主题; 2. 句子不要过长，不用描述一些很细节的东西; 3. 可以描述清楚它是什么牌子的，它是什么东西，它是干什么的。"""
+    #     )
+
+    # genai代理
+    res = genai_proxy(
+        url="https://gemini-openai-proxy2.zeabur.app/v1/chat/completions",
+        api_key=GENAI_API_KEY,
+        mode_name="gpt-4",
+        prompt="你是谁?"
+        )
     print(res)
+
--- opt.py
+++ opt.py
@@ -3,39 +3,46 @@
 '''
  Author: Sanfor Chow
  Date: 2024-04-25 20:19:58
- LastEditors: Sanfor Chow
- LastEditTime: 2024-04-25 20:19:59
+ LastEditors: courageux_san WX
+ LastEditTime: 2024-06-14 12:31:41
  FilePath: /script/opt.py
 '''
 import re
 import time
 from tqdm import tqdm
 import pandas as pd
-from my_lls import rewrite_text_with_genai
+from api.llm_groq import llm_groq
 
 
 
-def opt(data):
-    out_data = []
-    pbar = tqdm(total=len(data))
-    for i in data:
-        if isinstance(i[1], str):
-            # prompt = f'将“{i[1]}”里面的语气词改成“{i[0].split("——")[1]}”'
-            prompt = f'使用“{i[0].split("——")[1]}”这个语气助语编写五条关于“{i[2].split(".")[1]}”语境意思的句子, 要求五条句子不能重复相类'
-            res = rewrite_text_with_genai(text='', prompt=prompt)
-            # out_data.append(res)
-            out_data.append(i[2].strip() + "\t" + "\t".join(re.split(r'\d+\.+', res.replace('\n', ''), flags=re.MULTILINE)[1:]))
-            time.sleep(1)
-        pbar.update(1)
-    pbar.close()
 
-    return out_data
+# df = pd.read_excel('Appen014语料模板.xlsx', sheet_name='Sheet1')
+# df['语气词'] = df['语气词'].fillna(method='ffill')
+# out_data = opt(data=df[['语气词', '语料', '语气特征（细分编号）']].values)
 
-df = pd.read_excel('Appen014语料模板.xlsx', sheet_name='Sheet1')
-df['语气词'] = df['语气词'].fillna(method='ffill')
-out_data = opt(data=df[['语气词', '语料', '语气特征（细分编号）']].values)
+data = open('test_spell_error_5月1日后发布.txt', 'r', encoding='utf-8').readlines()
+data = [i.strip() for i in data]
 
-with open('tmp_B.txt', 'w') as file:
-    for fruit in out_data:
-        file.write(fruit + '\n')
+# out = []
+# for i in tqdm(data, desc="Processing data"):
+#     prompt = f'蒙古文本纠错,只返回修正后的文本, 保留原先的符号:\n{i}'
+#     r = llm_groq(text=prompt)
+#     # print(r)
+#     # for i in r.split("\n"):
+#     #     if '"' in i:
+#     #         print(i)
+#     out.append(r)
+#     # break
+
+out = []
+for i, item in enumerate(tqdm(range(len(data)), desc="Processing data"), 1):
+    if i % 50 == 0:
+        prompt = '蒙古文本纠错,只返回修正后的文本,返回结果一行对应一行,保留原先的符号:\n{}'.format("\n".join(data[i-50 : i]))
+        r = llm_groq(text=prompt)
+        for x in r.split('\n')[2:]:
+            out.append(x)
+
+with open('tmp.txt', 'w', encoding='utf-8') as file:
+    for o in out:
+        file.write(o + '\n')
 file.close()
