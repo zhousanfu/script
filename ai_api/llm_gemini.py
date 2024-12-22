9723e00ab511e6358ca64c18b9bbcc61365c64ff
sanford <courageux_san@wechat.com>
2024-12-22 23:20:00 +0800

    update

--- ai_api/llm_gemini.py
+++ ai_api/llm_gemini.py
@@ -3,12 +3,11 @@
 '''
  Author: Sanfor Chow
  Date: 2024-04-25 20:18:20
- LastEditors: courageux_san WX
- LastEditTime: 2024-07-31 11:13:47
- FilePath: /script/api/llm_gemini.py
+LastEditors: sanford courageux_san@wechat.com
+LastEditTime: 2024-12-22 23:19:17
+FilePath: /script/ai_api/llm_gemini.py
 '''
 import os
-import tqdm
 import requests
 import PIL.Image
 from io import BytesIO
@@ -75,13 +74,12 @@ def split_text_into_chunks(text, max_length=ai_max_length):
     chunks.append(current_chunk)
     return chunks
 
-# genai直连 文字
-def rewrite_text_with_genai(text, prompt="Please rewrite this text:"):
+# geminni 直连 文字
+def rewrite_text_with_genai(text, model_name="gemini-pro", prompt="Please rewrite this text:"):
     chunks = split_text_into_chunks(text)
     rewritten_text = ''
-    pbar = tqdm(total=len(chunks), ncols=150)
     genai.configure(api_key=GENAI_API_KEY)
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel(model_name)
     for chunk in chunks:
         _prompt=f"{prompt}\n{chunk}",
         response = model.generate_content(
@@ -116,12 +114,11 @@ def rewrite_text_with_genai(text, prompt="Please rewrite this text:"):
         for _chunk in response:
             if _chunk.text is not None:
                 rewritten_text += _chunk.text.strip()
-        pbar.update(1)
-    pbar.close()
+
     return rewritten_text
 
-# genai直连 多媒体
-def img_genai(image_url, prompt):
+# gemini 直连 多媒体
+def gemini_media(image_url, prompt):
     r = requests.get(image_url)
     image = PIL.Image.open(BytesIO(r.content))
     image.save('api/image2.png', 'PNG')
@@ -134,8 +131,8 @@ def img_genai(image_url, prompt):
 
     return response.text
 
-# genai直连 langchain_google_genai
-def gemini_chat(text):
+# gemini 直连 langchain
+def gemini_langchain_chat(text, model_name="gemini-pro"):
     chat_template = ChatPromptTemplate.from_messages([
             SystemMessage(content=("")),
             HumanMessagePromptTemplate.from_template("{text}"),
@@ -146,8 +143,8 @@ def gemini_chat(text):
     result = model(chat_message)
     print(result.content)
 
-# genai代理
-def genai_proxy(url, mode_name, prompt):
+# gemini 代理 文字
+def gemini_chat_proxy(url, prompt, mode_name="gpt-4"):
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer "+ GENAI_API_KEY
@@ -162,17 +159,17 @@ def genai_proxy(url, mode_name, prompt):
 
 
 if __name__ == '__main__':
-    # res = rewrite_text_with_genai(text='', prompt='你是谁')
+    res = rewrite_text_with_genai(text='', prompt='你是谁')
 
-    # res = img_genai(
+    # res = gemini_media(
     #     image_url='https://mmilabel-1258344707.cos.ap-guangzhou.myqcloud.com/upload/unpack/cardli/20240507/image/1005129185_b1d78a1a84f376cdf76980885253c666.jpg_ocrmt_new.jpg',
     #     prompt="""你需要使用一句英文句子来总结图片中的核心内容, 句子类似于图片标题一样的, 如果是一些产品图片只用说明产品是什么就行。 要求: 1. 句子要简洁明了，能够体现出主图片的主题; 2. 句子不要过长，不用描述一些很细节的东西; 3. 可以描述清楚它是什么牌子的，它是什么东西，它是干什么的。"""
     #     )
 
-    res = genai_proxy(
-        url="https://gemini-openai-proxy2.zeabur.app/v1/chat/completions",
-        mode_name="gpt-4",
-        prompt="你是谁?"
-        )
-    print(res)
+    # res = gemini_chat_proxy(
+    #     url="https://gemini-openai-proxy2.zeabur.app/v1/chat/completions",
+    #     mode_name="gpt-4",
+    #     prompt="你是谁?"
+    #     )
+    # print(res)
 
